9ae38764c6e2bfa3b13370d8473ef52b72d51a94
Chris Stewart <mcstewart76@gmail.com>
2023-02-06 10:34:08 -0500

    Adding icons to header
    some config updates and added icons to header

--- portfolio-next/components/Header.tsx
+++ portfolio-next/components/Header.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
-export default function Header({}: Props) {
-  return (
-    <header>Header Text
-        <div>
-            {/* Socials(github, linkedin, share) */}
-              <SocialIcon fgColor='grey' bgColor='transparent' url="https://www.linkedin.com/in/chris-stewart-79b283204/" />
-              <SocialIcon url="https://github.com/mcstewart76" />
-        </div>
-    </header>
-  )
+export default function Header({ }: Props) {
+    return (
+        <header className='sticky flex top-0 mt-3 items-start justify-between mx-auto xl:items-center max-w-7xl'>
+            <motion.div className='flex flex-row items-center'>
+                {/* Socials(github, linkedin, share) */}
+                <SocialIcon className='' fgColor='transparent' bgColor='grey' url="https://www.linkedin.com/in/chris-stewart-79b283204/" />
+                <SocialIcon className='' fgColor='grey' bgColor='transparent' url="https://github.com/mcstewart76" />
+            </motion.div>
+            <motion.div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+                <SocialIcon className='' fgColor='grey' bgColor='transparent' network='email' url='mailto:mcstewart76@gmail.com' />
+                <p className='hidden md:inline-flex text-sm text-gray-400'>Email me!</p>
+            </motion.div>
+        </header>
+    )
 }
