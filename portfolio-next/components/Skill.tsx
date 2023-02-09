c5a53a8dc865e07ce60e88b75d45d847ae09371c
Chris Stewart <mcstewart76@gmail.com>
2023-02-09 12:10:34 -0500

    roughed out skills section, changing to grid for all viewports

--- portfolio-next/components/Skill.tsx
+++ portfolio-next/components/Skill.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-type Props = {skillsRight: boolean}
+type Props = { skillsRight: boolean }
 
 function Skill({ skillsRight }: Props) {
     return (
         <div className='group relative flex cursor-pointer'>
             <motion.img src='https://avatars.githubusercontent.com/u/90533949?s=96&v=4' alt='about-img'
-            initial={{
-                opacity: 0,
-                x: skillsRight ? 300 : -300,
-            }}
-            transition={{ duration: 1.2 }}
-            whileInView={{opacity: 1, x: 0}}
+                className='rounded-full object-cover h-16 w-16 xl:h-32 xl:w-32 border-zinc-500 border filter
+            group-hover:grayscale transition duration-500 ease-in-out'
+                initial={{
+                    opacity: 0,
+                    x: skillsRight ? 200 : -200,
+                }}
+                transition={{ duration: 1.6 }}
+                whileInView={{ opacity: 1, x: 0 }}
             />
+            <div className='absolute opacity-0 group-hover:opacity-70 transition duration-300 ease-in-out group-hover:bg-white
+                           h-16 w-16 xl:h-32 xl:w-32 rounded-full z-0'>
+                <div className='flex items-center justify-center h-full'>
+                    <p className='text-2xl font-bold text-black'>
+                        80%
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- portfolio-next/components/Skills.tsx
+++ portfolio-next/components/Skills.tsx
@@ -1,14 +1,40 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Skill from './Skill'
 
 type Props = {}
 
 function Skills({ }: Props) {
     return (
-        <motion.div className='h-screen flex relative flex-col text-center md:text-left justify-center items-center xl:flex-row'>
+        <motion.div className='h-screen flex relative flex-col text-center md:text-left justify-center min-h-screen space-y-0 items-center xl:flex-row'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        >
             <h3 className='absolute top-24 uppercase tracking-[20px] text-2xl ml-3.5 text-zinc-500'>
                 Skills
             </h3>
+            <h3 className='absolute top-36 uppercase tracking-widest text-base text-zinc-500'>
+                Hover a skill for proficiency
+            </h3>
+            <div className='grid grid-cols-4 gap-5'>
+                <Skill skillsRight={false} />
+                <Skill skillsRight={false} />
+                <Skill skillsRight={false} />
+                <Skill skillsRight={false} />
+                <Skill skillsRight={false} />
+                <Skill skillsRight={false} />
+                <Skill skillsRight={false} />
+                <Skill skillsRight={false} />
+                <Skill skillsRight={true} />
+                <Skill skillsRight={true} />
+                <Skill skillsRight={true} />
+                <Skill skillsRight={true} />
+                <Skill skillsRight={true} />
+                <Skill skillsRight={true} />
+                <Skill skillsRight={true} />
+                <Skill skillsRight={true} />
+            </div>
         </motion.div>
     )
 }
