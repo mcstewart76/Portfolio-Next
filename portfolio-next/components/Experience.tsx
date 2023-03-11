b00225ce48d3aa33f3fa20262f45669b4c8ff67b
Chris Stewart <mcstewart76@gmail.com>
2023-03-10 23:19:54 -0500

    formatting on experience sections

--- portfolio-next/components/Experience.tsx
+++ portfolio-next/components/Experience.tsx
@@ -4,28 +4,28 @@ import ExperienceCard from './ExperienceCard';
 
 type Props = {};
 
-function Experience({}: Props) {
-  return (
-    <motion.div
-      className="h-screen grid sm:grid-cols-[1fr_6fr_1fr] grid-cols-[24px_1fr_6fr_1fr_24px] grid-rows-[48px_1fr_4fr_2fr_48px]
+function Experience({ }: Props) {
+    return (
+        <motion.div
+            className="h-screen grid sm:grid-cols-[1fr_6fr_1fr] grid-cols-[24px_1fr_6fr_1fr_24px] grid-rows-[48px_1fr_4fr_2fr_48px]
                 relative overflow-hidden md:flex-row max-w-full mx-auto items-center "
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1.6 }}
-    >
-      <div className="col-span-6 row-start-2 text-center">
-        <h3 className="uppercase tracking-[20px] text-2xl ml-4 text-zinc-500 text-center">
-          Experience
-        </h3>
-      </div>
-      <div className="flex overflow-x-scroll space-x-6 snap-x snap-mandatory col-start-1 col-end-5 sm:col-start-2 sm:col-end-3
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.6 }}
+        >
+            <div className="col-span-6 row-start-2 text-center">
+                <h3 className="uppercase tracking-[20px] text-2xl ml-4 text-zinc-500 text-center">
+                    Experience
+                </h3>
+            </div>
+            <div className="flex overflow-x-scroll space-x-6 snap-x snap-mandatory col-start-1 col-end-5 sm:col-start-2 sm:col-end-3
                      row-start-3 row-end-6">
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-      </div>
-    </motion.div>
-  );
+                <ExperienceCard />
+                <ExperienceCard />
+                <ExperienceCard />
+            </div>
+        </motion.div>
+    );
 }
 
 export default Experience;
--- portfolio-next/components/ExperienceCard.tsx
+++ portfolio-next/components/ExperienceCard.tsx
@@ -3,47 +3,47 @@ import { motion } from 'framer-motion';
 
 type Props = {};
 
-function ExperienceCard({}: Props) {
-  return (
-    <article
-      className="flex flex-col rounded-lg items-center space-y-7 overflow-scroll-y sm:flex-shrink-0
+function ExperienceCard({ }: Props) {
+    return (
+        <article
+            className="flex flex-col rounded-lg items-center space-y-7 overflow-scroll-y sm:flex-shrink-0
                  snap-center bg-zinc-700 px-10 py-4"
-    >
-      <motion.img
-        className="rounded-full h-32 w-32 object-cover xl:w-100 xl:h-100"
-        src="https://i.seadn.io/gae/JtcNSW3E6V94rwCnmt-sRUyXwvE9mLU5n9MZ15XMDDbI_S8mf3QI7GKU3f1ldDGZXl14ss9BMWbEJaYcYYAAB-yfBQn0sJOzzNs-NQ?auto=format&w=1000"
-        alt=""
-        initial={{
-          y: -25,
-          opacity: 0,
-        }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 1 }}
-        viewport={{ once: true }}
-      />
-      <div className="px-0 md:px-10 flex flex-col justify-start ">
-        <h4 className="text-4xl ">Future Executive</h4>
-        <p className="font-semibold text-3xl mt-1">Google</p>
-        <div className="flex space-x-2 my-2">
-          <img
-            className="h-10 w-10 object-cover rounded-full"
-            src="https://img.icons8.com/stickers/512/css3.png"
-            alt=""
-          />
-          {/* tech used */}
-          {/* tech used */}
-          {/* tech used */}
-        </div>
-        <p className="uppercase py-4">January 2030 - December 2034</p>
-        <ul className="list-disc grid-cols-2 space-y-4 ml-5 text-md">
-          <li>Work description</li>
-          <li>Work description</li>
-          <li>Work description</li>
-          <li>Work description Work but as this changes</li>
-        </ul>
-      </div>
-    </article>
-  );
+        >
+            <motion.img
+                className="rounded-full h-32 w-32 object-cover xl:w-100 xl:h-100"
+                src="https://i.seadn.io/gae/JtcNSW3E6V94rwCnmt-sRUyXwvE9mLU5n9MZ15XMDDbI_S8mf3QI7GKU3f1ldDGZXl14ss9BMWbEJaYcYYAAB-yfBQn0sJOzzNs-NQ?auto=format&w=1000"
+                alt=""
+                initial={{
+                    y: -25,
+                    opacity: 0,
+                }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 1 }}
+                viewport={{ once: true }}
+            />
+            <div className="px-0 md:px-10 flex flex-col justify-start ">
+                <h4 className="text-4xl ">Future Executive</h4>
+                <p className="font-semibold text-3xl mt-1">Google</p>
+                <div className="flex space-x-2 my-2">
+                    <img
+                        className="h-10 w-10 object-cover rounded-full"
+                        src="https://img.icons8.com/stickers/512/css3.png"
+                        alt=""
+                    />
+                    {/* tech used */}
+                    {/* tech used */}
+                    {/* tech used */}
+                </div>
+                <p className="uppercase py-4">January 2030 - December 2034</p>
+                <ul className="list-disc grid-cols-2 space-y-4 ml-5 text-md">
+                    <li>Work description</li>
+                    <li>Work description</li>
+                    <li>Work description</li>
+                    <li>Work description Work but as this changes</li>
+                </ul>
+            </div>
+        </article>
+    );
 }
 
 export default ExperienceCard;
